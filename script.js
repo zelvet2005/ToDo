1f5f0eef5cbb0df0b521791eb8c350ce604014e0
Vetal <vitalikzeleneckij3@gmail.com>
2024-07-16 18:29:15 +0300

    added opportunity to delete specific task

--- script.js
+++ script.js
@@ -4,28 +4,54 @@ const tasks = document.querySelector(".tasks");
 const addTaskInput = document.querySelector(".add-task-input");
 const addTaskBtn = document.querySelector(".add-task-btn");
 
+const separator = "^@&#&@^";
+
 const tasksKeyLS = "tasks";
-const separator = "^";
+const counterKeyLS = "count";
+
 const tasksArr = localStorage.getItem(tasksKeyLS)
   ? localStorage.getItem(tasksKeyLS).split(separator)
   : [];
+let counter = localStorage.getItem(counterKeyLS)
+  ? localStorage.getItem(counterKeyLS)
+  : 0;
 
 localStorage.setItem(tasksKeyLS, tasksArr.join(separator));
+localStorage.setItem(counterKeyLS, counter);
+
+const createTaskElement = function (taskValue, id) {
+  const taskHtml = `
+    <div class="task margin-bottom">
+      <input class="task-value" type="text" value="${taskValue}" />
+      <button class="complete-btn">&#10004;</button>
+      <button class="delete-btn" id="${id}">&#128465;</button>
+    </div>
+    `;
+  tasks.insertAdjacentHTML("beforeend", taskHtml);
+};
 
 const updateUI = function () {
   tasks.innerHTML = "";
 
-  tasksArr.forEach(function (task) {
-    const taskHtml = `
-    <div class="task margin-bottom">
-      <input class="task-value" type="text" value="${task}" />
-      <button class="complete-btn">&#10004;</button>
-      <button class="delete-btn">&#128465;</button>
-    </div>
-    `;
+  tasksArr.forEach(function (taskValue) {
+    const id = `delete-${counter}`;
+    createTaskElement(taskValue, id);
 
-    tasks.insertAdjacentHTML("beforeend", taskHtml);
+    document
+      .querySelector(`#${id}`)
+      .addEventListener("click", function (event) {
+        const parentTask = event.target.parentElement;
+        const taskTextForDelete = parentTask.querySelector("input").value;
+        const indexForDelete = tasksArr.indexOf(taskTextForDelete);
+
+        tasksArr.splice(indexForDelete, 1);
+        updateUI();
+
+        localStorage.setItem(tasksKeyLS, tasksArr.join(separator));
+      });
+    counter++;
   });
+  localStorage.setItem(counterKeyLS, counter);
 };
 
 addTaskBtn.addEventListener("click", function (event) {
